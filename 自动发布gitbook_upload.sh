38874f5ad79dd5ddb1827fa89b0f1bf5ee8e3b24
tanhuang <tanhuang1985@hotmail.com>
2021-11-06 23:02:37 +0800

    更新自动发布gitbook的shell脚本

--- 自动发布gitbook_upload.sh
+++ 自动发布gitbook_upload.sh
@@ -4,4 +4,8 @@ if [ -f /home/administrator/.bashrc ];then
 elif [ -f /home/tanhuang/.bashrc ];then
     . /home/tanhuang/.bashrc
 fi
-rsync -a --delete -e 'ssh -p 20022' /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+if [ `hostname` == "tanhuang-PC" ];then
+    rsync -a --delete -e 'ssh -p 22' /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+else
+    rsync -a --delete -e 'ssh -p 20022' /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+fi
