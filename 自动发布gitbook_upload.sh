cc4c60ebd0a23ae7b9f15e5dd5c204c5ced6c636
tanhuang <tanhuang1985@hotmail.com>
2021-12-18 00:56:12 +0800

    更新远程唤醒

--- 自动发布gitbook_upload.sh
+++ 自动发布gitbook_upload.sh
@@ -4,8 +4,10 @@ if [ -f /home/administrator/.bashrc ];then
 elif [ -f /home/tanhuang/.bashrc ];then
     . /home/tanhuang/.bashrc
 fi
-if [ `hostname` == "tanhuang-PC" ];then
-    rsync -a --delete -e 'ssh -p 22' /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+if [ "$(hostname)" == "tanhuang-PC" ];then
+    rsync -a --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 22' \
+    /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
 else
-    rsync -a --delete -e 'ssh -p 20022' /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+    rsync -a --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 20022' \
+    /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
 fi
