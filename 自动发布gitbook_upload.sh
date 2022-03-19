69a2ee52c00794a57ef93771fd13f76f8150b2bb
unknown <407474568@qq.com>
2022-03-19 14:10:23 +0800

    该用户不屑于输入备注

--- 自动发布gitbook_upload.sh
+++ 自动发布gitbook_upload.sh
@@ -5,9 +5,9 @@ elif [ -f /home/tanhuang/.bashrc ];then
     . /home/tanhuang/.bashrc
 fi
 if [ "$(hostname)" == "tanhuang-PC" ];then
-    rsync -a --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 22' \
+    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 22' \
     /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
 else
-    rsync -a --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 20022' \
+    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 20022' \
     /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
 fi
