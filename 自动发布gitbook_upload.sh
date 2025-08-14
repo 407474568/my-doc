c50d5f3f3bb45962627f76c6b00352e9dc23c5f9
tanhuang <407474568@qq.com>
2025-08-14 22:02:44 +0800

    该用户不屑于输入备注

--- 自动发布gitbook_upload.sh
+++ 自动发布gitbook_upload.sh
@@ -5,9 +5,11 @@ elif [ -f /home/tanhuang/.bashrc ];then
     . /home/tanhuang/.bashrc
 fi
 if [ "$(hostname)" == "tanhuang-PC" ];then
-    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 22' \
-    /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+#    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 22' \
+    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator.MICROSO-DBHB3B7/.ssh/id_ed25519 -p 22' \
+    /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
 else
-    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000' \
-    /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+#    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000' \
+    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator.MICROSO-DBHB3B7/.ssh/id_ed25519 -p 6000' \
+    /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
 fi
