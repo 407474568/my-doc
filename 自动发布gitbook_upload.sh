77b74a8f881fe744d9f09f74cd475911eeffcb74
tanhuang <407474568@qq.com>
2025-09-06 15:55:55 +0800

    该用户不屑于输入备注

--- 自动发布gitbook_upload.sh
+++ 自动发布gitbook_upload.sh
@@ -6,10 +6,14 @@ elif [ -f /home/tanhuang/.bashrc ];then
 fi
 if [ "$(hostname)" == "tanhuang-PC" ];then
 #   rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 22' \
-    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator.MICROSO-DBHB3B7/.ssh/id_ed25519 -p 22' \
-    /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
+#    rsync -aP --delete -e 'ssh -i ~/.ssh/id_ed25519 -p 6000' \
+#    /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
+# 再次修改, 注意要把 密钥文件放到 mintty 的家目录下, 否则权限映射转换问题难得折腾
+    rsync -aP --delete -e 'ssh -p 6000' /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
 else
 #    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000' \
     rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator.MICROSO-DBHB3B7/.ssh/id_ed25519 -p 6000' \
     /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
 fi
+
+sleep 6000
