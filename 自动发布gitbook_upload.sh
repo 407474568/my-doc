2afe9b91c21f52277b5611931a188755cc046719
tanhuang <407474568@qq.com>
2025-09-06 16:07:17 +0800

    该用户不屑于输入备注

--- 自动发布gitbook_upload.sh
+++ 自动发布gitbook_upload.sh
@@ -11,7 +11,7 @@ if [ "$(hostname)" == "tanhuang-PC" ];then
 # 再次修改, 注意要把 密钥文件放到 mintty 的家目录下, 否则权限映射转换问题难得折腾
     rsync -aP --delete -e 'ssh -p 6000' /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
 elif [ "$(hostname)" == "tanhuang-note" ];then
-#    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000' \
+#    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000'
     rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000' \
     /cygdrive/D/Code/my-doc/_book/ root@code.heyday.net.cn:/docker/my-doc-book --debug=ALL
 fi
