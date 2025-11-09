df2027920f001bd39a2aa7d9cd4dded75c4c3277
tanhuang <407474568@qq.com>
2025-11-09 22:06:50 +0800

    该用户不屑于输入备注

--- 自动发布book.cmd
+++ 自动发布book.cmd
@@ -36,4 +36,6 @@ REM git commit -m %title%
 REM git push -f git master:gh-pages
 
 REM "D:\Program Files\cygwin64\bin\mintty.exe" bash D:\Code\my-doc\自动发布gitbook_upload.sh -l D:\TEMP\mintty.log
-"D:\Program Files\cygwin64\bin\bash.exe" -lc "/cygdrive/d/Code/my-doc/自动发布gitbook_upload.sh -l /cygdrive/d/TEMP/mintty.log"
+"D:\Program Files\cygwin64\bin\bash.exe" -lc "/cygdrive/d/Code/my-doc/自动发布gitbook_upload.sh" ^
+    > D:\TEMP\自动发布gitbook_upload.log 2>&1
+type D:\TEMP\自动发布gitbook_upload.log
