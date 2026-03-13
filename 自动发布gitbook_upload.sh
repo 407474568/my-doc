bc27a4ecbf29f8b38b4d0eb0ed0ff4be706915f0
tanhuang <407474568@qq.com>
2026-03-13 12:30:08 +0800

    该用户不屑于输入备注

--- 自动发布gitbook_upload.sh
+++ 自动发布gitbook_upload.sh
@@ -14,7 +14,7 @@ if [ "$(hostname)" == "tanhuang-PC" ] || [ "$(hostname)" == "tanhuang-PC-CQ" ];t
     # 是因为 code.heyday.net.cn 被解析到的 192.168.100.10
     # 而 www.heyday.net.cn 才是 192.168.100.30
     # 不这样做的结果就是把静态html会推送到 git 仓库, 而不是 docker 节点
-    rsync -aP --delete -e 'ssh -p 6000' /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
+    rsync -aP --delete /cygdrive/D/Code/my-doc/_book/ root@www.heyday.net.cn:/docker/my-doc-book --debug=ALL
 elif [ "$(hostname)" == "tanhuang-note" ];then
 #    rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000'
     rsync -aP --delete -e 'ssh -i /cygdrive/c/Users/Administrator/.ssh/id_ed25519 -p 6000' \
